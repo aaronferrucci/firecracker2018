de88271f1846b1e1795a438d7e116b2fe7edc5ec
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2018-07-05 18:07:33 -0700

    Add 5k plot.

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # firecracker2018
-![alt text](https://github.com/aaronferrucci/firecracker2018/blob/master/time_vs_age.svg "SVG image")
+![alt text](https://github.com/aaronferrucci/firecracker2018/blob/master/time_vs_age5k.svg "5k time vs. age image")
+![alt text](https://github.com/aaronferrucci/firecracker2018/blob/master/time_vs_age10k.svg "10k time vs. age image")
 
 To reproduce these results: 
 1. The race data is [here](https://runsignup.com/Race/Results/47158/?rsus=100-200-fe20fdef-2ac6-4a9b-ba22-de732186e749#resultSetId-121795)
   * I resorted to displaying all results, selecting, copying, and pasting into
-    a file (data/raw_10k)
+    a file (data/raw_10k, data/raw5k)
   * data/Makefile cleans up the raw data, to a form suitable for read.table.
 2. Load the project in RStudio
 3. source("firecracker2018.R")
 
-From there you can export the plot, or experiment further with the data set.
+From there you can export the plots, or experiment further with the data set.
 
 This data set is rather messy (like all race data sets I've seen). Here are
-some issues I noticed:
+some issues I noticed in the 10k data:
 1. Of the 319 entries, 207 lack their bib number.
 2. Gender seems wrong in some cases. Example: the 2nd place winner, "Luke
    Colosi", is marked as female. (Luke's Division is "M 19-24", though, so the
--- firecracker2018.R
+++ firecracker2018.R
@@ -22,11 +22,30 @@ hoursMinutesSeconds <- strsplit(data10k$ClockTime, ":")
 data10k$Time <- sapply(hoursMinutesSeconds, function(hms) Reduce(function(acc, x) as.numeric(acc) * 60 + as.numeric(x), hms ))
 time_ticks <- seq(0, max(data10k$Time), 10 * 60)
 
-plot <-
+plot10k <-
   ggplot(data10k, aes(x = Age, y = Time, color=Gender)) +
+  ggtitle("Firecracker 10k") +
+  theme(plot.title = element_text(hjust = 0.5)) +
   expand_limits(x = 0, y = 30*60) +
   scale_y_continuous(breaks = time_ticks, labels = timestr(time_ticks), name = "elapsed time (h:mm:ss)") +
   stat_smooth(formula = y~x) +
   geom_point()
 
-print(plot)
+print(plot10k)
+
+data5k <- read.table("data/processed_5k.txt", sep="\t", stringsAsFactors=F)
+names(data5k) <- c("Place", "Bib", "FirstName", "LastName", "Gender", "City", "State", "Country", "ClockTime", "ChipTime", "Pace", "Age", "AgePercent", "DivisionPlace", "Division")
+hoursMinutesSeconds <- strsplit(data5k$ClockTime, ":")
+data5k$Time <- sapply(hoursMinutesSeconds, function(hms) Reduce(function(acc, x) as.numeric(acc) * 60 + as.numeric(x), hms ))
+time_ticks <- seq(0, max(data5k$Time), 10 * 60)
+
+plot5k <-
+  ggplot(data5k, aes(x = Age, y = Time, color=Gender)) +
+  ggtitle("Firecracker 5k") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  expand_limits(x = 0, y = 10*60) +
+  scale_y_continuous(breaks = time_ticks, labels = timestr(time_ticks), name = "elapsed time (h:mm:ss)") +
+  stat_smooth(formula = y~x) +
+  geom_point()
+
+print(plot5k)

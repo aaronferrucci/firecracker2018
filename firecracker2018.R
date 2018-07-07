c1c691a7fd594210b3bc553ae3f1e1e56ef615c8
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2018-07-07 14:44:02 -0700

    Add division background to plots.

--- firecracker2018.R
+++ firecracker2018.R
@@ -19,33 +19,61 @@ timestr <- function(elapsed) {
 data10k <- read.table("data/processed_10k.txt", sep="\t", stringsAsFactors=F)
 names(data10k) <- c("Place", "Bib", "FirstName", "LastName", "Gender", "City", "State", "Country", "ClockTime", "ChipTime", "Pace", "Age", "AgePercent", "DivisionPlace", "Division")
 hoursMinutesSeconds <- strsplit(data10k$ClockTime, ":")
+# Two records are missing "Age". Making my best guess.
+data10k[!is.na(data10k$Bib) & data10k$Bib == 482,]$Age = 61
+data10k[!is.na(data10k$Bib) & data10k$Bib == 549,]$Age = 63
+
 data10k$Time <- sapply(hoursMinutesSeconds, function(hms) Reduce(function(acc, x) as.numeric(acc) * 60 + as.numeric(x), hms ))
 time_ticks <- seq(5 * 60, max(data10k$Time), 10 * 60)
+age_ticks <- seq(10, max(data10k$Age, na.rm=T), 10)
 
+divisions = data.frame(xmin=c(13, 25, 35, 45, 55, 65, 75), xmax=c(18, 29, 39, 49, 59, 69, max(data10k$Age, na.rm=T) + 1), ymin=c(-Inf), ymax=c(Inf))
 plot10k <-
-  ggplot(data10k, aes(x = Age, y = Time, color=Gender)) +
+  ggplot(data10k) +
+  geom_rect(data=divisions, aes(xmin=xmin, xmax=xmax,ymin=ymin,ymax=ymax), fill="moccasin",linetype=0, alpha=0.3) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black")) +
+  geom_point(aes(x=Age, y=Time, color=Gender)) +
   ggtitle("Firecracker 10k") +
   theme(plot.title = element_text(hjust = 0.5)) +
-  expand_limits(x = 0, y = 35*60) +
+  expand_limits(x = 10, y = 35*60) +
   scale_y_continuous(breaks = time_ticks, labels = timestr(time_ticks), name = "elapsed time (h:mm:ss)") +
-  stat_smooth(formula = y~x) +
-  geom_point()
+  scale_x_continuous(breaks = age_ticks) +
+  geom_smooth(data=data10k, method="loess", aes(x=Age, y=Time, color=Gender), formula = y~x)
 
 print(plot10k)
+svg(filename="time_vs_age10k.svg")
+print(plot10k)
+dev.off()
 
 data5k <- read.table("data/processed_5k.txt", sep="\t", stringsAsFactors=F)
 names(data5k) <- c("Place", "Bib", "FirstName", "LastName", "Gender", "City", "State", "Country", "ClockTime", "ChipTime", "Pace", "Age", "AgePercent", "DivisionPlace", "Division")
 hoursMinutesSeconds <- strsplit(data5k$ClockTime, ":")
 data5k$Time <- sapply(hoursMinutesSeconds, function(hms) Reduce(function(acc, x) as.numeric(acc) * 60 + as.numeric(x), hms ))
 time_ticks <- seq(5 * 60, max(data5k$Time), 10 * 60)
+age_ticks <- seq(0, max(data5k$Age, na.rm=T), 10)
 
+divisions = data.frame(xmin=c(13, 25, 35, 45, 55, 65, 75), xmax=c(18, 29, 39, 49, 59, 69, max(data5k$Age, na.rm=T) + 1), ymin=c(-Inf), ymax=c(Inf))
 plot5k <-
-  ggplot(data5k, aes(x = Age, y = Time, color=Gender)) +
+  ggplot(data5k) +
+  geom_rect(data=divisions, aes(xmin=xmin, xmax=xmax,ymin=ymin,ymax=ymax), fill="moccasin",linetype=0, alpha=0.3) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black")) +
+  geom_point(aes(x=Age, y=Time, color=Gender)) +
   ggtitle("Firecracker 5k") +
   theme(plot.title = element_text(hjust = 0.5)) +
   expand_limits(x = 0, y = 15*60) +
   scale_y_continuous(breaks = time_ticks, labels = timestr(time_ticks), name = "elapsed time (h:mm:ss)") +
-  stat_smooth(formula = y~x) +
-  geom_point()
+  scale_x_continuous(breaks = age_ticks) +
+  geom_smooth(data=data5k, method="loess", aes(x=Age, y=Time, color=Gender), formula = y~x)
 
 print(plot5k)
+svg(filename="time_vs_age5k.svg")
+print(plot5k)
+dev.off()

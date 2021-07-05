7c05fe1bd24576ae94fe8f3735ec7d12c20a6781
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2021-07-05 13:09:12 -0700

    2021 5k results

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# firecracker2019
+# firecracker2021
 ![alt text](https://github.com/aaronferrucci/firecracker2018/blob/2019/time_vs_age5k.svg "5k time vs. age image")
-![alt text](https://github.com/aaronferrucci/firecracker2018/blob/2019/time_vs_age10k.svg "10k time vs. age image")
 
 To reproduce these results: 
 1. The race data is [here](https://runsignup.com/Race/Results/47158/?rsus=100-200-fe20fdef-2ac6-4a9b-ba22-de732186e749#resultSetId-161091;perpage:10)
   * I resorted to displaying all results, selecting, copying, and pasting into
-    a file (data/raw_10k, data/raw5k)
+    a file (data/raw_5k)
   * data/Makefile cleans up the raw data, to a form suitable for read.table.
 2. Load the project in RStudio
 3. source("firecracker2018.R")
 
 From there you can export the plots, or experiment further with the data set.
 
-Data cleanup: the data has a few imcomplete records, which I've removed.
 
--- firecracker2018.R
+++ firecracker2018.R
@@ -16,39 +16,6 @@ timestr <- function(elapsed) {
   return(time)
 }
 
-data10k <- read.table("data/processed_10k.txt", sep="\t", quote="", stringsAsFactors=F)
-names(data10k) <- c("Place", "Bib", "FirstName", "LastName", "Gender", "City", "State", "Country", "ClockTime", "ChipTime", "Pace", "Age", "AgePercent", "DivisionPlace", "Division")
-# Manually data cleaning
-data10k <- na.omit(data10k)
-
-hoursMinutesSeconds <- strsplit(data10k$ClockTime, ":")
-
-data10k$Time <- sapply(hoursMinutesSeconds, function(hms) Reduce(function(acc, x) as.numeric(acc) * 60 + as.numeric(x), hms ))
-time_ticks <- seq(5 * 60, max(data10k$Time), 10 * 60)
-age_ticks <- seq(10, max(data10k$Age, na.rm=T), 10)
-
-divisions = data.frame(xmin=c(13, 25, 35, 45, 55, 65, 75), xmax=c(18, 29, 39, 49, 59, 69, max(data10k$Age, na.rm=T) + 1), ymin=c(-Inf), ymax=c(Inf))
-plot10k <-
-  ggplot(data10k) +
-  geom_rect(data=divisions, aes(xmin=xmin, xmax=xmax,ymin=ymin,ymax=ymax), fill="moccasin",linetype=0, alpha=0.3) +
-  theme(
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    axis.line = element_line(colour = "black")) +
-  geom_point(aes(x=Age, y=Time, color=Gender)) +
-  ggtitle("Firecracker 10k") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  expand_limits(x = 10, y = 35*60) +
-  scale_y_continuous(breaks = time_ticks, labels = timestr(time_ticks), name = "elapsed time (h:mm:ss)") +
-  scale_x_continuous(breaks = age_ticks) +
-  geom_smooth(data=data10k, method="loess", aes(x=Age, y=Time, color=Gender), formula = y~x)
-
-print(plot10k)
-svg(filename="time_vs_age10k.svg")
-print(plot10k)
-dev.off()
-
 data5k <- read.table("data/processed_5k.txt", sep="\t", quote="", stringsAsFactors=F)
 names(data5k) <- c("Place", "Bib", "FirstName", "LastName", "Gender", "City", "State", "Country", "ClockTime", "ChipTime", "Pace", "Age", "AgePercent", "DivisionPlace", "Division")
 # Manually data cleaning

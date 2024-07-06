e88c866b1d86e6c9967ce3ef778d875450641618
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2024-07-05 20:35:58 -0700

    2024

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# firecracker2022
-![alt text](https://github.com/aaronferrucci/firecracker2018/blob/2022/time_vs_age5k.svg "5k time vs. age image")
-![alt text](https://github.com/aaronferrucci/firecracker2018/blob/2022/time_vs_age10k.svg "10k time vs. age image")
+# firecracker2024
+![alt text](https://github.com/aaronferrucci/firecracker2024/blob/2022/time_vs_age10k.svg "10k time vs. age image")
 
-To reproduce these results: 
-1. The race data is [here](https://runsignup.com/Race/Results/47158#resultSetId-325566;perpage:5000)
-  * I resorted to displaying all results, selecting, copying, and pasting into
-    a file (data/raw*)
-  * data/Makefile cleans up the raw data, to a form suitable for read.table.
-2. Load the project in RStudio
-3. source("firecracker2018.R")
-
-From there you can export the plots, or experiment further with the data set.
 
---
+++ main.R
@@ -0,0 +1,69 @@
+
+library(ggplot2)
+library(testit)
+
+timestr <- function(elapsed) {
+  seconds <- elapsed
+  hours <- as.integer(seconds / 3600)
+  seconds <- seconds - hours * 3600
+  minutes <- as.integer(seconds / 60)
+  seconds <- round(seconds - minutes * 60, digits=2)
+
+  minute_prefix <- ifelse(minutes < 10, "0", "")
+  minutes <- paste0(minute_prefix, minutes)
+  second_prefix <- ifelse(seconds < 10, "0", "")
+  seconds <- paste0(second_prefix, seconds)
+
+  time <- paste(hours, minutes, seconds, sep=":")
+  return(time)
+}
+
+# manually clean the data. This works for 2022 data format, perhaps for future years also.
+cleanit <- function(data, tag) {
+  # two columns are all N/A, remove them (but check, first)
+  assert(paste0(tag, ": column 13 is not all NA"), all(is.na(data[,13])))
+  data <- data[-13]
+  assert(paste0(tag, ": column 11 is not all NA"), all(is.na(data[,11])))
+  data <- data[-11]
+  
+  # column 3 is just an initial. Remove it
+  assert(paste0(tag, ": column 3 isn't all single initial"), all(nchar(data$V3) == 1))
+  data <- data[-3]
+
+  return(data)
+}
+
+divisions = data.frame(xmin=c(13, 25, 35, 45, 55, 65, 75) - 0.5, xmax=c(18, 29, 39, 49, 59, 69, 79) + 0.5, ymin=c(-Inf), ymax=c(Inf))
+
+data10k <- read.table("data/firecracker_10k_2024_manually_modified.txt", sep="\t", quote="", stringsAsFactors=F)
+data10k <- cleanit(data10k, "data10k")
+names(data10k) <- c("Place", "Bib", "Name", "Gender", "City", "State", "Country", "ClockTime", "ChipTime", "Age", "DivisionPlace", "Division")
+
+hoursMinutesSeconds <- strsplit(data10k$ChipTime, ":")
+data10k$Time <- sapply(hoursMinutesSeconds, function(hms) Reduce(function(acc, x) as.numeric(acc) * 60 + as.numeric(x), hms ))
+time_ticks <- seq(5 * 60, max(data10k$Time), 10 * 60)
+age_ticks <- seq(10, max(data10k$Age, na.rm=T), 10)
+
+plot10k <-
+  ggplot(data10k) +
+  geom_rect(data=divisions, aes(xmin=xmin, xmax=xmax,ymin=ymin,ymax=ymax), fill="moccasin",linetype=0, alpha=0.3) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black")) +
+  geom_point(aes(x=Age, y=Time, color=Gender)) +
+  ggtitle("Firecracker 10k") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  expand_limits(x = 10, y = 35*60) +
+  scale_y_continuous(breaks = time_ticks, labels = timestr(time_ticks), name = "elapsed time (h:mm:ss)") +
+  scale_x_continuous(breaks = age_ticks) +
+  geom_smooth(data=data10k, method="loess", aes(x=Age, y=Time, color=Gender), formula = y~x)
+
+extra <- data10k[data10k$Name == "Aaron Ferrucci",]
+plot10k <- plot10k + geom_point(data=extra, aes(x = Age, y = Time))
+
+print(plot10k)
+svg(filename="time_vs_age10k.svg")
+print(plot10k)
+dev.off()
